a62b20b155d6221301f5d7e1259f27a98c74f83f
tapion <tapion@tapion.it>
2013-01-04 12:29:51 +0100

    / ‘db.sql’

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS ambiente (
   cap VARCHAR(45),
   numero_civico VARCHAR(45),
   nome VARCHAR(45) NOT NULL 
-)
+);
 
 CREATE TABLE IF NOT EXISTS utente (
   email VARCHAR(45) NOT NULL PRIMARY KEY,
@@ -13,7 +13,7 @@ CREATE TABLE IF NOT EXISTS utente (
   cognome VARCHAR(45) NOT NULL ,
   password VARCHAR(45) NOT NULL ,
   username VARCHAR(45) NOT NULL 
-)
+);
 
 CREATE TABLE IF NOT EXISTS appartenenza (
   id_ambiente INTEGER NOT NULL ,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS appartenenza (
   PRIMARY KEY (id_ambiente, id_utente) ,
   FOREIGN KEY (id_ambiente) REFERENCES ambiente(id_ambiente),
   FOREIGN KEY (id_utente) REFERENCES utente(email)
-)
+);
 
 CREATE TABLE IF NOT EXISTS spesa (
   id_spesa INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS spesa (
   cliente VARCHAR(45) NOT NULL,
   FOREIGN KEY(ambiente) REFERENCES ambiente(id_ambiente),
   FOREIGN KEY(cliente) REFERENCES utente(email)
-)
+);
 
 CREATE TABLE IF NOT EXISTS pagamento (
   id_pagamento INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -43,7 +43,7 @@ CREATE TABLE IF NOT EXISTS pagamento (
   id_creditore VARCHAR(45) NOT NULL ,
   FOREIGN KEY (id_pagante) REFERENCES utente(email),
   FOREIGN KEY (id_creditore) REFERENCES utente(email),
-)
+);
 
 CREATE TABLE IF NOT EXISTS prodotto (
   id_prodotto INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -54,7 +54,7 @@ CREATE TABLE IF NOT EXISTS prodotto (
   codice_a_barre INTEGER ,
   spesa INTEGER NOT NULL ,
   FOREIGN KEY (spesa) REFERENCES spesa(id_spesa)
-)
+);
 
 CREATE TABLE IF NOT EXISTS utilizzo (
   prodotto INTEGER NOT NULL ,
@@ -62,7 +62,7 @@ CREATE TABLE IF NOT EXISTS utilizzo (
   PRIMARY KEY (`prodotto`, `utente`) ,
   FOREIGN KEY (prodotto) REFERENCES prodotto(id_prodotto),
   FOREIGN KEY (utente) REFERENCES utente(email),
-)
+);
 
 CREATE TABLE IF NOT EXISTS commento (
   id_commento INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -72,4 +72,4 @@ CREATE TABLE IF NOT EXISTS commento (
   id_utente VARCHAR(45) NOT NULL ,
   FOREIGN KEY (id_prodotto) REFERENCES prodotto(id_prodotto),
   FOREIGN KEY (id_utente) REFERENCES email(utente),
-)
+);
