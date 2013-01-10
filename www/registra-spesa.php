585ac342eacb8b625151c71156b64837aaa7336b
tapion <tapion@tapion.it>
2013-01-10 17:45:06 +0100

    / ‘www/registra-spesa.php’

--- www/registra-spesa.php
+++ www/registra-spesa.php
@@ -1,3 +1,9 @@
-<?php
-
-?>
+<form method="post" action="">
+	<input type="text" name="negozio" placeholder="negozio"/>
+	<input type="date" name="data" placeholder="data"/>
+	<p>Prodotti</p>
+	<input type="text" name="nome" placeholder="nome"/>
+	<input type="text" name="quantita" placeholder="quantita"/>
+	<input type="text" name="costo" placeholder="costo"/>
+	<input type="text" name="descrizione" placeholder="descrizione"/>
+</form>
