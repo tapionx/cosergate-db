a45e4598681c64a5806c556ff28bbd4a24cfc6b9
tapion <tapion@tapion.it>
2013-01-10 16:05:32 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -3,8 +3,17 @@
 session_start();
 
 if(!isset($_SESSION['loggato']) || (!isset($_GET['ambiente'])))
+	header('Location: index.php');
 	die('furbacchione');
 
-if(!isset($_GET['ambiente'])
+$query = "SELECT * FROM utente WHERE email='$email';";
+	$esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
+	if(mysql_num_rows($esiste) == 0){
+		$query = "INSERT INTO utente VALUES('$email', '$nome', '$cognome', '$password', '$nomeutente');";
+		mysql_query($query, $db) or die('Errore nella INSERT');
+		header("Location: index.php");
+	} else {
+		echo "<h1>Questa email è già registrata.</h1>";
+	}
 
 ?>
