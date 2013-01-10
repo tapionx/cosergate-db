147074d6d8183d8c57c6942028255a44cb2d7ec4
tapion <tapion@tapion.it>
2013-01-10 19:04:43 +0100

    / ‘www/registra-spesa.php’

--- www/registra-spesa.php
+++ www/registra-spesa.php
@@ -11,11 +11,11 @@ if(!isset($_SESSION['loggato'])){
 if(isset($_POST['inseriscispesa'])){
 	
 	$inserisci_spesa = "INSERT INTO spesa (negozio, ambiente, cliente) VALUES ('{$_POST['negozio']}', {$_GET['ambiente']}, '{$_SESSION['email']}');";
-	mysql_query($inserisci_spesa, $db) or die('Errore nella INSERT SPESA');
+	mysql_query($inserisci_spesa, $db) or die("Errore nella INSERT SPESA: $inserisci_spesa");
 	
 	for($i=0;$i<$_POST['nprodotti'];$i++){
 		$inserisci_prodotto = "INSERT INTO prodotto () VALUES ();";
-		mysql_query($inserisci_spesa, $db) or die('Errore nella INSERT PRODOTTO');
+		mysql_query($inserisci_spesa, $db) or die("Errore nella INSERT PRODOTTO: $inserisci_prodotto");
 	}
 }
 
