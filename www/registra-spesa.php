4018639f8316420dc87c74c5a3d3c82c765dc38e
tapion <tapion@tapion.it>
2013-01-10 17:48:10 +0100

    / ‘www/registra-spesa.php’

--- www/registra-spesa.php
+++ www/registra-spesa.php
@@ -2,10 +2,21 @@
 	<input type="text" name="negozio" placeholder="negozio"/>
 	<input type="date" name="data" placeholder="data"/>
 	<p>Prodotti</p>
-	<input type="text" name="nome" placeholder="nome"/>
-	<input type="text" name="quantita" placeholder="quantita"/>
-	<input type="text" name="costo" placeholder="costo"/>
-	<input type="text" name="descrizione" placeholder="descrizione"/>
+	<input type="text" name="nome-1" placeholder="nome"/>
+	<input type="text" name="quantita-1" placeholder="quantita"/>
+	<input type="text" name="costo-1" placeholder="costo"/>
+	<input type="text" name="descrizione-1" placeholder="descrizione"/>
 	<br/>
+	<input type="text" name="nome-2" placeholder="nome"/>
+	<input type="text" name="quantita-2" placeholder="quantita"/>
+	<input type="text" name="costo-2" placeholder="costo"/>
+	<input type="text" name="descrizione-2" placeholder="descrizione"/>
+	<br/>
+	<input type="text" name="nome-3" placeholder="nome"/>
+	<input type="text" name="quantita-3" placeholder="quantita"/>
+	<input type="text" name="costo-3" placeholder="costo"/>
+	<input type="text" name="descrizione-3" placeholder="descrizione"/>
+	<br/>
+	<input type="hidden" name="nprodotti" value="3"/>
 	<input type="submit" nome="inseriscispesa" value="Inserisci"/>
 </form>
