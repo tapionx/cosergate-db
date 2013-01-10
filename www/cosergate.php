8870961e930c4e2244d4319d0984812367276bca
tapion <tapion@tapion.it>
2013-01-10 16:25:39 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -14,7 +14,7 @@ if(mysql_num_rows($esiste) == 0){
 	die('2');
 }
 
-$query = "SELECT * FROM appartenenza WHERE id_ambiente='{$_GET['ambiente']}' AND id_utente='${_SESSION['email']}';";
+$query = "SELECT * FROM appartenenza WHERE id_ambiente='{$_GET['ambiente']}' AND id_utente='{$_SESSION['email']}';";
 $esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 if(mysql_num_rows($esiste) == 0){
 	die('3');
