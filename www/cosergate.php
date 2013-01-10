750604d7994fb955a32adab93753e3c2fefb19b4
tapion <tapion@tapion.it>
2013-01-10 17:06:52 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -23,7 +23,7 @@ if(mysql_num_rows($esiste) == 0){
 
 <p>Il resoconto</p>
 <?php
-$nutenti = "SELECT count() FROM appartenenza WHERE id_ambiente={$_GET['ambiente']};";
+$query = "SELECT count() FROM appartenenza WHERE id_ambiente={$_GET['ambiente']};";
 $nutenti = mysql_query($nutenti, $db) or die("Errore nella SELECT: '$query'");
 print_r($nutenti);
 
