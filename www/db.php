35d5404a738897c23de748314453e579ffa62b76
tapion <tapion@tapion.it>
2013-01-10 17:24:32 +0100

    / ‘www/db.php’

--- www/db.php
+++ www/db.php
@@ -6,9 +6,9 @@ mysql_fetch_all
 Absurdly simple but utilitarian function returns a numeric array of associative arrays containing an entire result set.
 */
 function mysql_fetch_all($result) {
-$all = array();
-while ($all[] = mysql_fetch_assoc($result)) {}
-return $all;
+	$all = array();
+	while ($all[] = mysql_fetch_assoc($result)) {}
+	return $all;
 }
 
 $db = mysql_connect('localhost','cosergate','cosergate') or die("Errore di connessione al DB");
