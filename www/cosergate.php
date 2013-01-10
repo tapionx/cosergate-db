cb0e58963715541df6ecfa162e6707fac88d162b
tapion <tapion@tapion.it>
2013-01-10 16:09:13 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -9,9 +9,7 @@ if(!isset($_SESSION['loggato']) || (!isset($_GET['ambiente'])))
 $query = "SELECT * FROM ambiente WHERE id_ambiente='${$_GET['ambiente']}';";
 $esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 if(mysql_num_rows($esiste) == 0){
-	$query = "INSERT INTO utente VALUES('$email', '$nome', '$cognome', '$password', '$nomeutente');";
-	mysql_query($query, $db) or die('Errore nella INSERT');
-	header("Location: index.php");
+	die();
 } else {
 	echo "<h1>Questa email è già registrata.</h1>";
 }
