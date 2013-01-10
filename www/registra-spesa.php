a11d74e78dd273b18e9f774e0457ac7d09e581d2
tapion <tapion@tapion.it>
2013-01-10 20:37:16 +0100

    / ‘www/registra-spesa.php’

--- www/registra-spesa.php
+++ www/registra-spesa.php
@@ -45,17 +45,7 @@ if(isset($_POST['inseriscispesa'])){
 	<input type="text" name="quantita-1" placeholder="quantita"/>
 	<input type="text" name="costo-1" placeholder="costo"/>
 	<input type="text" name="descrizione-1" placeholder="descrizione"/>
-	<br/>
-	<input type="text" name="nome-2" placeholder="nome"/>
-	<input type="text" name="quantita-2" placeholder="quantita"/>
-	<input type="text" name="costo-2" placeholder="costo"/>
-	<input type="text" name="descrizione-2" placeholder="descrizione"/>
-	<br/>
-	<input type="text" name="nome-3" placeholder="nome"/>
-	<input type="text" name="quantita-3" placeholder="quantita"/>
-	<input type="text" name="costo-3" placeholder="costo"/>
-	<input type="text" name="descrizione-3" placeholder="descrizione"/>
-	<br/>
+	<input type="button" name="aggiungiriga" value="Aggiungi prodotto" />
 	<input type="hidden" name="nprodotti" value="3"/>
 	<input type="submit" name="inseriscispesa" value="Inserisci"/>
 </form>
