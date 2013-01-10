8dc2c1f4fe2bb4605a3370e16ebbbf578b1e817f
tapion <tapion@tapion.it>
2013-01-10 16:23:17 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -4,18 +4,18 @@ session_start();
 
 if(!isset($_SESSION['loggato']) || (!isset($_GET['ambiente'])))
 	header('Location: index.php');
-	die();
+	die('1');
 
 $query = "SELECT * FROM ambiente WHERE id_ambiente='${$_GET['ambiente']}';";
 $esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 if(mysql_num_rows($esiste) == 0){
-	die();
+	die('2');
 }
 
 $query = "SELECT * FROM appartenenza WHERE id_ambiente='${$_GET['ambiente']}' AND id_utente='${_SESSION['email']}';";
 $esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 if(mysql_num_rows($esiste) == 0){
-	die();
+	die('3');
 }
 ?>
 
