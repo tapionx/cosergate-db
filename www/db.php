8ab8492c483f5b4dac6bc159a1fc986ad8a7e3f1
tapion <tapion@tapion.it>
2013-01-10 17:24:26 +0100

    / ‘www/db.php’

--- www/db.php
+++ www/db.php
@@ -1,4 +1,16 @@
 <?php
+
+/*
+mysql_fetch_all
+ 
+Absurdly simple but utilitarian function returns a numeric array of associative arrays containing an entire result set.
+*/
+function mysql_fetch_all($result) {
+$all = array();
+while ($all[] = mysql_fetch_assoc($result)) {}
+return $all;
+}
+
 $db = mysql_connect('localhost','cosergate','cosergate') or die("Errore di connessione al DB");
 mysql_select_db("cosergate", $db) or die("select db");
 
