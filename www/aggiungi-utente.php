6aa177a7f29a0913117e0e5114f633d37e80e77c
Luca Regazzi <luca.regazzi@gmail.com>
2013-01-11 11:46:46 +0100

    / ‘www/aggiungi-utente.php’

--- www/aggiungi-utente.php
+++ www/aggiungi-utente.php
@@ -1,5 +1,6 @@
 <?php 
 session_start();
+require_once('db.php');
 
 if(!isset($_SESSION['loggato'])){
 		die("Non Loggato.");
@@ -10,4 +11,20 @@ if(!isset($_GET['ambiente'])){
 		die("Nessun ambiente selezionato.");
 		echo '<a href="lista-ambienti.php">Seleziona un ambiente.</a>';
 }
+
+/* Controllo se esiste l'ambiente */
+
+$query = "SELECT * FROM ambiente WHERE id_ambiente={$_GET['ambiente']}";
+$esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
+if(mysql_num_rows($esiste) == 0){
+	die('2');
+}
+ /* controllo se l'utente appartiene all'ambiente */
+ 
+$query = "SELECT * FROM appartenenza WHERE id_ambiente={$_GET['ambiente']} AND id_utente='{$_SESSION['email']}'";
+$esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
+if(mysql_num_rows($esiste) == 0){
+	die('3');
+}
+
 ?>
