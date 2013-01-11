13a5a87c1bcf89fa33b428c4432e145b45e946b5
tapion <tapion@tapion.it>
2013-01-11 10:44:55 +0100

    / ‘www/db.php’

--- www/db.php
+++ www/db.php
@@ -1,7 +1,7 @@
 <?php
 
 
-global($db) = mysql_connect('localhost','cosergate','cosergate') or die("Errore di connessione al DB");
+$db = mysql_connect('localhost','cosergate','cosergate') or die("Errore di connessione al DB");
 mysql_select_db("cosergate", $db) or die("select db");
 
 /*
