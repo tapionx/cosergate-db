9d3d57ca7dcce97c896de45b27419e2c23ab4dc2
tapion <tapion@tapion.it>
2013-01-11 11:06:41 +0100

    / ‘www/registra-spesa.php’

--- www/registra-spesa.php
+++ www/registra-spesa.php
@@ -79,22 +79,21 @@ $utenti = query("SELECT * FROM utente JOIN appartenenza ON utente.email=apparten
 		?>
 		<th>Descrizione</th>
 	</tr>
-	<tr>
-		<div id="prodotti">
-			<input type="text" name="nome-1" placeholder="nome"/>
-			<input type="text" name="quantita-1" placeholder="quantita"/>
-			<input type="text" name="costo-1" placeholder="costo"/>
-			<?php
-			
-			foreach($utenti as $utente){
-				echo "<input type='checkbox' name='{$utente['email']}' checked/>";
-			}
-			
-			?>
-			<input type="text" name="descrizione-1" placeholder="descrizione"/>
-			<br />
-		</div>
-	</tr>
+	<div id="prodotti">
+		<tr><input type="text" name="nome-1" placeholder="nome"/></tr>
+		<tr><input type="text" name="quantita-1" placeholder="quantita"/></tr>
+		<tr><input type="text" name="costo-1" placeholder="costo"/></tr>
+		<?php
+		
+		foreach($utenti as $utente){
+			echo "<tr><input type='checkbox' name='{$utente['email']}' checked/></tr>";
+		}
+		
+		?>
+		<tr><input type="text" name="descrizione-1" placeholder="descrizione"/></tr>
+	</div>
+</table>
+
 	<input type="button" name="aggiungiriga" value="Aggiungi prodotto" onclick="aggiungi_prodotto()"/>
 	<input id="nprodotti" type="hidden" name="nprodotti" value="3"/>
 	<input type="submit" name="inseriscispesa" value="Inserisci"/>
