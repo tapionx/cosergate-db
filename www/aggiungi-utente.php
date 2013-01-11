84d8003986797c7207426fae12b986121f1128a9
Luca Regazzi <luca.regazzi@gmail.com>
2013-01-11 11:41:51 +0100

    / ‘www/aggiungi-utente.php’

--- www/aggiungi-utente.php
+++ www/aggiungi-utente.php
@@ -5,6 +5,11 @@ echo $_SESSION['loggato'];
 
 if(!isset($_SESSION['loggato'])){
 		die("Non Loggato.");
-		echo '<a href="index.html">Fai il login</a>';
+		echo '<a href="index.php">Fai il login</a>';
+}
+
+if(!isset($_GET['ambiente'])){
+		die("Nessun ambiente selezionato.");
+		echo '<a href="lista-ambienti.php">Seleziona un ambiente.</a>';
 }
 ?>
