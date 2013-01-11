faa610839ac384f21966c9c60cb6a63612cec2eb
tapion <tapion@tapion.it>
2013-01-11 16:38:23 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -44,6 +44,8 @@ $nutenti = mysql_fetch_assoc($nutenti)['nutenti'];
 
 $utenti = query("SELECT username, saldo FROM appartenenza JOIN utente ON appartenenza.id_utente=utente.id_utente WHERE id_ambiente={$_GET['ambiente']};");
 
+print_r($utenti);
+
 $query = "SELECT count(id_utente) AS nutenti FROM appartenenza WHERE id_ambiente={$_GET['ambiente']};";
 $nutenti = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 $nutenti = mysql_fetch_assoc($nutenti)['nutenti'];
