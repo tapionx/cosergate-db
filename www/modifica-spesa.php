4151c09ab631e432c750d3b6b5581b29ad02b93e
tapion <tapion@tapion.it>
2013-01-11 18:16:18 +0100

    / ‘www/modifica-spesa.php’

--- www/modifica-spesa.php
+++ www/modifica-spesa.php
@@ -3,6 +3,7 @@
 require_once("db.php");
 
 $spesa = query("SELECT * FROM spesa WHERE id_spesa={$_POST['id_spesa']}");
+$prodotti = query("SELECT * FROM prodotto WHERE id_spesa={$_POST['id_spesa']}");
 header("Content-type:text/plain;");
 print_r($spesa);
 ?>
