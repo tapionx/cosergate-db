3bef6c0e3e3626e195596fa4aafa60b7a7432b47
tapion <tapion@tapion.it>
2013-01-11 10:49:01 +0100

    / ‘www/registra-spesa.php’

--- www/registra-spesa.php
+++ www/registra-spesa.php
@@ -56,7 +56,7 @@ if(isset($_POST['inseriscispesa'])){
 // Recupero il numero di utenti
 $nutenti = query("SELECT count(id_utente) AS nutenti FROM appartenenza WHERE id_ambiente='{$_GET['ambiente']}';");
 
-$utenti = query("SELECT * FROM utenti JOIN appartenenza ON utenti.email=appartenenza.id_utente WHERE id_ambiente='{$_GET['ambiente']}';");
+$utenti = query("SELECT * FROM utente JOIN appartenenza ON utente.email=appartenenza.id_utente WHERE id_ambiente='{$_GET['ambiente']}';");
 
 print_r($nutenti);
 print_r($utenti);
