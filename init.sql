941f84a055456cc661dedebdef59795ef7ad4049
tapion <tapion@tapion.it>
2013-01-11 15:50:30 +0100

    / ‘init.sql’

--- init.sql
+++ init.sql
@@ -2,10 +2,10 @@ DROP DATABASE cosergate;
 CREATE DATABASE cosergate;
 USE cosergate;
 source /home/tapion/SparkleShare/cosergate-db/db.sql
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('tapion@tapion.it', 'Riccardo', 'Serafini', 'e07910a06a086c83ba41827aa00b26ed', 'tapion');
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('daniele@sciuto.it', 'Daniele', 'Sciuto', 'e07910a06a086c83ba41827aa00b26ed', 'sciuto');
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('dario@serra.it', 'Dario', 'Serra', 'e07910a06a086c83ba41827aa00b26ed', 'daffo');
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('luca@regazzi.it', 'Luca', 'Regazzi', 'e07910a06a086c83ba41827aa00b26ed', 'okone');
+INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('tapion@tapion.it', 'Riccardo', 'Serafini', '7815696ecbf1c96e6894b779456d330e', 'tapion');
+INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('daniele@sciuto.it', 'Daniele', 'Sciuto', '7815696ecbf1c96e6894b779456d330e', 'sciuto');
+INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('dario@serra.it', 'Dario', 'Serra', '7815696ecbf1c96e6894b779456d330e', 'daffo');
+INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('luca@regazzi.it', 'Luca', 'Regazzi', '7815696ecbf1c96e6894b779456d330e', 'okone');
 INSERT INTO ambiente (nome) VALUES ('prova');
 INSERT INTO appartenenza (id_ambiente, id_utente, saldo) VALUES (1, 'tapion@tapion.it', 0);
 INSERT INTO appartenenza (id_ambiente, id_utente, saldo) VALUES (1, 'daniele@sciuto.it', 0);
