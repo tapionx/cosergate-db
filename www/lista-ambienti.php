44ae2f6c6ac4e1b00daee7c8280a2534f6ebdb38
tapion <tapion@tapion.it>
2013-01-11 15:35:26 +0100

    / ‘www/lista-ambienti.php’

--- www/lista-ambienti.php
+++ www/lista-ambienti.php
@@ -4,21 +4,23 @@ session_start();
 if(isset($_GET['logout'])){
 	 session_unset();
 	 header('Location: index.php');
+	 die();
 }
 
-if(isset($_SESSION['loggato'])){
-	$email = $_SESSION['email'];
-	echo "<p>Benvenuto ".$email.'</p>';
-	require_once('db.php');
-	$ambienti = query("SELECT * FROM appartenenza JOIN ambiente ON appartenenza.id_ambiente=ambiente.id_ambiente WHERE id_utente='$email'");
-	foreach($ambienti as $ambiente){
-		echo '<a href="cosergate.php?ambiente='.$ambiente['id_ambiente'].'" >'.$ambiente['nome'].'</a>';
-	}
-	
-	echo "<p><a href='registra-ambiente.php'>Crea un nuovo ambiente</a></p>";
-	echo '<a href="cosergate.php?logout=1">Logout</a>';
-	
-} else {
-	echo "Non sei loggato";
+if(!isset($_SESSION['loggato'])){
+	header('Location: index.php');
+	die();
 }
+
+$email = $_SESSION['email'];
+echo "<p>Benvenuto ".$email.'</p>';
+require_once('db.php');
+$ambienti = query("SELECT * FROM appartenenza JOIN ambiente ON appartenenza.id_ambiente=ambiente.id_ambiente WHERE id_utente='$email'");
+foreach($ambienti as $ambiente){
+	echo '<a href="cosergate.php?ambiente='.$ambiente['id_ambiente'].'" >'.$ambiente['nome'].'</a>';
+}
+
+echo "<p><a href='registra-ambiente.php'>Crea un nuovo ambiente</a></p>";
+echo '<a href="cosergate.php?logout=1">Logout</a>';
+	
 ?>
