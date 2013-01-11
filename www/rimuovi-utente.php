2c65f8a1c3c442b0818d9b2d93f05ad42e040264
Luca Regazzi <luca.regazzi@gmail.com>
2013-01-11 12:52:45 +0100

    / ‘www/rimuovi-utente.php’

--- www/rimuovi-utente.php
+++ www/rimuovi-utente.php
@@ -29,28 +29,10 @@ if(mysql_num_rows($esiste) == 0){
 
 if(isset($_POST['rimuovi'])){
 	
-	if($email != $_SESSION['email']){ /* Controllo se sto aggiungendo me stesso */
-		$query = "SELECT * FROM utente WHERE email='$email'";
+		$query = "DELETE FROM appartenenza WHERE id_utente='".$_SESSION['email']."' AND id_ambiente=".$_GET['ambiente']." AND totale=0";
 		$utente = mysql_query($query, $db) or die('Errore nella SELECT');
 
 		if( mysql_num_rows($utente) == 1){ /* controllo se la mail esiste */
-			
-			$query = "SELECT * FROM appartenenza WHERE id_utente='$email' AND id_ambiente=".$_GET['ambiente'];
-			$utente = mysql_query($query, $db) or die('Errore nella select');
-			
-			if(mysql_num_rows($utente) == 0){ /* controllo se la mail non esite già nell'ambiente */
-				$query = "INSERT INTO appartenenza VALUES('{$_GET['ambiente']}','$email',0)";
-				$utente = mysql_query($query, $db) or die('Errore nell\'inserimento dell\'utente');
-				echo "Utente aggiunto!";
-			}else{
-					echo "Utente già presente";
-			}
-		} else {
-			echo "Email errata.";
-		}
-	}else{
-		echo "Non puoi aggiungere te stesso furbacchione. =)";
-	}
 }
 ?>
 
