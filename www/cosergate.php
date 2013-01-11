02666ec616684bdda0da6a46d41cc8e062530a61
Luca Regazzi <luca.regazzi@gmail.com>
2013-01-11 11:45:58 +0100

    / ‘www/cosergate.php’

--- www/cosergate.php
+++ www/cosergate.php
@@ -13,12 +13,14 @@ if(!isset($_SESSION['loggato']) || (!isset($_GET['ambiente']))) {
 	die('1');
 }
 
+/* Controllo se esiste l'ambiente */
+
 $query = "SELECT * FROM ambiente WHERE id_ambiente={$_GET['ambiente']}";
 $esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 if(mysql_num_rows($esiste) == 0){
 	die('2');
 }
-
+ /* controllo se l'utente appartiene all'ambiente */
 $query = "SELECT * FROM appartenenza WHERE id_ambiente={$_GET['ambiente']} AND id_utente='{$_SESSION['email']}'";
 $esiste = mysql_query($query, $db) or die("Errore nella SELECT: '$query'");
 if(mysql_num_rows($esiste) == 0){
