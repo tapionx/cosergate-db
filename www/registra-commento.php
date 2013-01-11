cfffed64e9144bb7b013d8cc0a727f42d86a287d
tapion <tapion@tapion.it>
2013-01-11 17:32:56 +0100

    / ‘www/registra-commento.php’

--- www/registra-commento.php
+++ www/registra-commento.php
@@ -1,3 +1,6 @@
 <?php
+
+query("INSERT INTO commento (testo, id_prodotto, id_utente) VALUES ('{$_POST['commento']}',{$_POST['id_prodotto']},'{$_POST['id_utente']}')");
+
 echo "{$_POST['commento']} su {$_POST['id_prodotto']}";
 ?>
