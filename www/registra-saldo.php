689b04f03dfed809b2e224a60025bec461d62904
tapion <tapion@tapion.it>
2013-01-11 17:59:10 +0100

    / ‘www/registra-saldo.php’

--- www/registra-saldo.php
+++ www/registra-saldo.php
@@ -1,5 +1,5 @@
 <?php
 
-query("INSERT INTO pagamento (importo, id_pagante, id_creditore) VALUES ({$_POST['importo']}, {$_GET['ambiente']},{$_POST['creditore']});");
-
+query("INSERT INTO pagamento (importo, id_pagante, id_creditore) VALUES ({$_POST['importo']}, {$_GET['ambiente']}, {$_POST['creditore']});");
+header("Location: cosergate.php?ambiente={$_GET['ambiente']}");
 ?>
