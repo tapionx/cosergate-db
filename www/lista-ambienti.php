69359cdcbd7037fe3948ef3cd9f620a36797b081
tapion <tapion@tapion.it>
2013-01-11 15:23:25 +0100

    / ‘www/lista-ambienti.php’

--- www/lista-ambienti.php
+++ www/lista-ambienti.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['loggato'])){
 	$email = $_SESSION['email'];
 	echo "<p>Benvenuto ".$email.'</p>';
 	require_once('db.php');
-	$query = "SELECT * FROM appartenenza JOIN ambiente ON appartenenza.id_ambiente=ambiente.id_ambiente WHERE id_utente='$email'";
+	$query = "SELECT * FROM appartenenza JOIN ambiente ON appartenenza.id_ambiente=id_ambiente.id_ambiente WHERE id_utente='$email'";
 	$ambienti = mysql_query($query, $db) or die('Errore nella SELECT');
 	
 	if(mysql_num_rows($ambienti) == 0){
