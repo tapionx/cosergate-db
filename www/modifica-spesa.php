e636d9d1220b10b973228b67d088f63226241f93
tapion <tapion@tapion.it>
2013-01-11 19:22:20 +0100

    / ‘www/modifica-spesa.php’

--- www/modifica-spesa.php
+++ www/modifica-spesa.php
@@ -5,7 +5,10 @@ require_once("db.php");
 $spesa = query("SELECT * FROM spesa WHERE id_spesa={$_POST['id_spesa']}");
 $prodotti = query("SELECT * FROM prodotto WHERE id_spesa={$_POST['id_spesa']}");
 
-echo "<form method=''";
+echo "<form method='post' action=''>
+		<input type='text' name='negozio' value='{$_POST['negozio']}'/>
+		<input type='text' name='negozio' value='{$_POST['data']}'/>
+	  </form>";
 
 header("Content-type:text/plain;");
 print_r($spesa);
