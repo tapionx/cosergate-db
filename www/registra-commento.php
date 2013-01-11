948bec01250ad8a29aa444338b62740edd7c1c30
tapion <tapion@tapion.it>
2013-01-11 17:35:20 +0100

    / ‘www/registra-commento.php’

--- www/registra-commento.php
+++ www/registra-commento.php
@@ -1,8 +1,8 @@
 <?php
-
+session_start();
 require_once("db.php");
 
-query("INSERT INTO commento (testo, id_prodotto, id_utente) VALUES ('{$_POST['commento']}',{$_POST['id_prodotto']},'{$_POST['id_utente']}')");
+query("INSERT INTO commento (testo, id_prodotto, id_utente) VALUES ('{$_POST['commento']}',{$_POST['id_prodotto']},'{$_SESSION['email']}')");
 
 echo "{$_POST['commento']} su {$_POST['id_prodotto']}";
 ?>
