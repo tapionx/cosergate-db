062989ca8ed2debcbf9fdae13ee3a8c964effa9d
tapion <tapion@tapion.it>
2013-01-11 20:42:00 +0100

    / ‘www/modifica-spesa.php’

--- www/modifica-spesa.php
+++ www/modifica-spesa.php
@@ -4,7 +4,7 @@ require_once("db.php");
 
 $spesa = query("SELECT * FROM spesa WHERE id_spesa={$_POST['id_spesa']}")[0];
 $prodotti = query("SELECT * FROM prodotto WHERE id_spesa={$_POST['id_spesa']}");
-$utenti = query("SELECT * FROM utenti WHERE id_ambiente={$_GET['ambiente']}");
+$utenti = query("SELECT * FROM utenti WHERE id_ambiente={$_GET['ambiente']};");
 
 echo "<form method='post' action=''>
 		<input type='text' name='negozio' value='{$spesa['negozio']}' />
