59f3e3c88a754a5b330ae6c7b279170afb918f9a
tapion <tapion@tapion.it>
2013-01-11 17:36:06 +0100

    / ‘www/registra-commento.php’

--- www/registra-commento.php
+++ www/registra-commento.php
@@ -4,5 +4,5 @@ require_once("db.php");
 
 query("INSERT INTO commento (testo, id_prodotto, id_utente) VALUES ('{$_POST['commento']}',{$_POST['id_prodotto']},'{$_SESSION['email']}')");
 
-echo "{$_POST['commento']} su {$_POST['id_prodotto']}";
+header("Location: cosergate.php?ambiente={$_GET['ambiente']}");
 ?>
