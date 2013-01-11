1cbe704b21a0326168774077adfec7b57b7b3c19
tapion <tapion@tapion.it>
2013-01-11 18:04:41 +0100

    / ‘www/registra-saldo.php’

--- www/registra-saldo.php
+++ www/registra-saldo.php
@@ -4,5 +4,7 @@ session_start();
 require_once("db.php");
 
 query("INSERT INTO pagamento (importo, id_pagante, id_creditore) VALUES ({$_POST['importo']}, '{$_SESSION['email']}', '{$_POST['creditore']}');");
+query("UPDATE appartenenza SET saldo = saldo - {$_POST['importo']} WHERE id_utente='{$_POST['creditore']}' AND id_ambiente={$_GET['ambiente']}");
+query("UPDATE appartenenza SET saldo = saldo + {$_POST['importo']} WHERE id_utente='{$_SESSION['email']}' AND id_ambiente={$_GET['ambiente']}");
 header("Location: cosergate.php?ambiente={$_GET['ambiente']}");
 ?>
