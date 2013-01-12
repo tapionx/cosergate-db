aadddeb3a697e025248b017b8f0f3c74b0369d93
tapion <tapion@tapion.it>
2013-01-12 15:43:26 +0100

    / ‘www/index.php’

--- www/index.php
+++ www/index.php
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Cosergate</title>
+		
+	</head>
+	<body>
 <h1>COSERGATE</h1>
 <p>Non sei registrato? <a href="registra-utente.php">Registrati</a></p>
 <form method="post" action="login.php">
