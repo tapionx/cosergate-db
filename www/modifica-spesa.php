0af27c2758102ad79cd6ad70e9a44c7e3d99bc7b
tapion <tapion@tapion.it>
2013-01-12 16:05:02 +0100

    / ‘www/modifica-spesa.php’

--- www/modifica-spesa.php
+++ www/modifica-spesa.php
@@ -22,7 +22,7 @@ if(isset($_POST['spesamodificata'])){
 
 		$costo_per_utilizzatore_old = $prodotto_old['costo'] / count($utilizzi);
 		
-		query("UPDATE appartenenza SET saldo = saldo - {$prodotto_old['costo']} WHERE id_utente='{$prodotto_old['id_']}' AND id_ambiente={$_GET['ambiente']}");
+		query("UPDATE appartenenza SET saldo = saldo - {$prodotto_old['costo']} WHERE id_utente='{$_POST['id_utente']}' AND id_ambiente={$_GET['ambiente']}");
 		
 		
 		query("UPDATE prodotto SET nome='{$prodotto['nome']}', quantita={$prodotto['quantita']}, costo={$prodotto['costo']}, descrizione='{$prodotto['descrizione']}' WHERE id_prodotto=$id_prodotto");
