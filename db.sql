544c6c00e12e66998e9596ed637d0208eb4fcd95
tapion <tapion@tapion.it>
2013-01-12 12:59:25 +0100

    / ‘db.sql’

--- db.sql
+++ db.sql
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS spesa (
   timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   id_ambiente INTEGER NOT NULL,
   id_utente VARCHAR(45) NOT NULL,
-  FOREIGN KEY(id_ambiente) REFERENCES ambiente(id_ambiente),
+  FOREIGN KEY(id_ambiente) REFERENCES ambiente(id_ambiente) ON DELETE CASCADE,
   FOREIGN KEY(id_utente) REFERENCES utente(id_utente)
 );
 
