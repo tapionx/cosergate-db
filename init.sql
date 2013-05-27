270a4806f5bd7378349758ddb9cd1b8d898e7648
tapione <tapion@tapion.it>
2013-05-27 20:08:26 +0300

    uasd

--- init.sql
+++ init.sql
@@ -1,13 +1,1 @@
-DROP DATABASE cosergate;
-CREATE DATABASE cosergate;
-USE cosergate;
-source db.sql
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('tapion@tapion.it', 'Riccardo', 'Serafini', '7815696ecbf1c96e6894b779456d330e', 'tapion');
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('daniele@sciuto.it', 'Daniele', 'Sciuto', '7815696ecbf1c96e6894b779456d330e', 'sciuto');
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('dario@serra.it', 'Dario', 'Serra', '7815696ecbf1c96e6894b779456d330e', 'daffo');
-INSERT INTO utente (id_utente, nome, cognome, password, username) VALUES ('luca@regazzi.it', 'Luca', 'Regazzi', '7815696ecbf1c96e6894b779456d330e', 'okone');
-INSERT INTO ambiente (nome) VALUES ('prova');
-INSERT INTO appartenenza (id_ambiente, id_utente, saldo) VALUES (1, 'tapion@tapion.it', 0);
-INSERT INTO appartenenza (id_ambiente, id_utente, saldo) VALUES (1, 'daniele@sciuto.it', 0);
-INSERT INTO appartenenza (id_ambiente, id_utente, saldo) VALUES (1, 'dario@serra.it', 0);
-INSERT INTO appartenenza (id_ambiente, id_utente, saldo) VALUES (1, 'luca@regazzi.it', 0);
+a
